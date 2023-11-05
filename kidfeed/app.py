94f3660f9b28af6b1c4e4ef70781d5edae20abef
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-05 23:57:12 +0530

    Create kidfeed/app.py

---
+++ kidfeed/app.py
@@ -0,0 +1,34 @@
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sign Up</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<h1>Newsfeed for Kids</h1>
+<nav>
+<a href="/">Home</a>
+<a href="/login">Login</a>
+<a href="/signup">Sign Up</a>
+</nav>
+</header>
+
+<main>
+<section id="signup">
+<form action="/signup" method="post">
+<input type="text" name="username" placeholder="Username">
+<input type="password" name="password" placeholder="Password">
+<input type="submit" value="Sign Up">
+</form>
+</section>
+</main>
+
+<footer>
+<p>Copyright 2023 Newsfeed for Kids</p>
+</footer>
+</body>
+</html>
+```
