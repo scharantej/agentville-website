43b47d2df2ab3d4502cfd5c9d921486b6bf58c5a
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-06 15:09:43 +0530

    Update  Penny/app.py

---  Penny/app.py
+++  Penny/app.py
@@ -1,5 +1,5 @@
  
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,113 +8,65 @@ db = SQLAlchemy(app)
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Float, nullable=False)
-    category = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-
-    def __repr__(self):
-        return '<Expense %r>' % self.id
+    name = db.Column(db.String(80))
+    amount = db.Column(db.Float)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('expenses', lazy=True))
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Category %r>' % self.name
+    name = db.Column(db.String(50))
 
 @app.route('/')
 def index():
-    expenses = Expense.query.all()
-    return render_template('index.html', expenses=expenses)
+    return render_template('index.html')
 
 @app.route('/categories')
 def categories():
-    categories = Category.query.all()
-    return render_template('categories.html', categories=categories)
+    return render_template('categories.html')
 
 @app.route('/reports')
 def reports():
     return render_template('reports.html')
 
-@app.route('/add_expense', methods=['GET', 'POST'])
+@app.route('/add_expense', methods=['POST'])
 def add_expense():
-    if request.method == 'POST':
-        amount = request.form['amount']
-        category = request.form['category']
-        date = request.form['date']
-
-        expense = Expense(amount=amount, category=category, date=date)
-        db.session.add(expense)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('add_expense.html')
-
-@app.route('/edit_expense/<int:expense_id>', methods=['GET', 'POST'])
-def edit_expense(expense_id):
-    expense = Expense.query.get_or_404(expense_id)
-
-    if request.method == 'POST':
-        amount = request.form['amount']
-        category = request.form['category']
-        date = request.form['date']
-
-        expense.amount = amount
-        expense.category = category
-        expense.date = date
-
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('edit_expense.html', expense=expense)
-
-@app.route('/delete_expense/<int:expense_id>')
-def delete_expense(expense_id):
-    expense = Expense.query.get_or_404(expense_id)
-
-    db.session.delete(expense)
+    name = request.form['name']
+    amount = float(request.form['amount'])
+    category_id = int(request.form['category_id'])
+    expense = Expense(name=name, amount=amount, category_id=category_id)
+    db.session.add(expense)
     db.session.commit()
-
     return redirect(url_for('index'))
 
-@app.route('/add_category', methods=['GET', 'POST'])
-def add_category():
-    if request.method == 'POST':
-        name = request.form['name']
-
-        category = Category(name=name)
-        db.session.add(category)
+@app.route('/edit_expense/<int:id>', methods=['GET', 'POST'])
+def edit_expense(id):
+    expense = Expense.query.get_or_404(id)
+    if request.method == 'GET':
+        return render_template('edit_expense.html', expense=expense)
+    else:
+        expense.name = request.form['name']
+        expense.amount = float(request.form['amount'])
+        expense.category_id = int(request.form['category_id'])
         db.session.commit()
+        return redirect(url_for('index'))
 
-        return redirect(url_for('categories'))
-
-    return render_template('add_category.html')
-
-@app.route('/edit_category/<int:category_id>', methods=['GET', 'POST'])
-def edit_category(category_id):
-    category = Category.query.get_or_404(category_id)
-
-    if request.method == 'POST':
-        name = request.form['name']
-
-        category.name = name
-
-        db.session.commit()
-
-        return redirect(url_for('categories'))
-
-    return render_template('edit_category.html', category=category)
-
-@app.route('/delete_category/<int:category_id>')
-def delete_category(category_id):
-    category = Category.query.get_or_404(category_id)
-
-    db.session.delete(category)
+@app.route('/delete_expense/<int:id>')
+def delete_expense(id):
+    expense = Expense.query.get_or_404(id)
+    db.session.delete(expense)
     db.session.commit()
+    return redirect(url_for('index'))
 
-    return redirect(url_for('categories'))
+@app.route('/get_expenses')
+def get_expenses():
+    expenses = Expense.query.all()
+    return jsonify([expense.to_dict() for expense in expenses])
+
+@app.route('/get_categories')
+def get_categories():
+    categories = Category.query.all()
+    return jsonify([category.to_dict() for category in categories])
 
 if __name__ == '__main__':
     app.run(debug=True)
