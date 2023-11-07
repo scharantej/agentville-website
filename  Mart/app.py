f66e31794aa0f05f36ce43d6609e214dda46d26d
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 00:17:48 +0530

    Update  Mart/app.py

---  Mart/app.py
+++  Mart/app.py
@@ -7,60 +7,80 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 db = SQLAlchemy(app)
 
 class Product(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), unique=True, nullable=False)
-  price = db.Column(db.Float, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(120), nullable=True)
 
-  def __repr__(self):
-    return '<Product %r>' % self.name
-
-class CartItem(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-  product = db.relationship('Product', backref=db.backref('cart_items', lazy=True))
-  quantity = db.Column(db.Integer, nullable=False)
-
-  def __repr__(self):
-    return '<CartItem %r>' % self.product.name
+    def __repr__(self):
+        return '<Product %r>' % self.name
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    products = Product.query.all()
+    return render_template('index.html', products=products)
 
-@app.route('/products')
-def products():
-  products = Product.query.all()
-  return render_template('products.html', products=products)
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    product = Product.query.get_or_404(product_id)
+    return render_template('product_detail.html', product=product)
+
+@app.route('/add_to_cart/<int:product_id>')
+def add_to_cart(product_id):
+    product = Product.query.get_or_404(product_id)
+    cart = request.cookies.get('cart')
+    if cart:
+        cart = cart.split(',')
+        if product.id not in cart:
+            cart.append(str(product.id))
+        response = redirect(url_for('cart'))
+        response.set_cookie('cart', ','.join(cart))
+        return response
+    else:
+        response = redirect(url_for('cart'))
+        response.set_cookie('cart', str(product.id))
+        return response
 
 @app.route('/cart')
 def cart():
-  cart_items = CartItem.query.all()
-  return render_template('cart.html', cart_items=cart_items)
-
-@app.route('/add_to_cart', methods=['POST'])
-def add_to_cart():
-  product_id = request.form.get('product_id')
-  product = Product.query.get(product_id)
-  cart_item = CartItem(product=product, quantity=1)
-  db.session.add(cart_item)
-  db.session.commit()
-  return redirect(url_for('cart'))
-
-@app.route('/remove_from_cart', methods=['POST'])
-def remove_from_cart():
-  cart_item_id = request.form.get('cart_item_id')
-  cart_item = CartItem.query.get(cart_item_id)
-  db.session.delete(cart_item)
-  db.session.commit()
-  return redirect(url_for('cart'))
+    cart = request.cookies.get('cart')
+    if cart:
+        cart = cart.split(',')
+        products = Product.query.filter(Product.id.in_(cart))
+        return render_template('cart.html', products=products)
+    else:
+        return render_template('cart.html', products=[])
 
 @app.route('/checkout')
 def checkout():
-  cart_items = CartItem.query.all()
-  total_price = 0
-  for cart_item in cart_items:
-    total_price += cart_item.product.price * cart_item.quantity
-  return render_template('checkout.html', cart_items=cart_items, total_price=total_price)
+    cart = request.cookies.get('cart')
+    if cart:
+        cart = cart.split(',')
+        products = Product.query.filter(Product.id.in_(cart))
+        total_price = 0
+        for product in products:
+            total_price += product.price
+        return render_template('checkout.html', products=products, total_price=total_price)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/confirmation', methods=['POST'])
+def confirmation():
+    cart = request.cookies.get('cart')
+    if cart:
+        cart = cart.split(',')
+        products = Product.query.filter(Product.id.in_(cart))
+        for product in products:
+            product.stock -= 1
+            db.session.add(product)
+        db.session.commit()
+        response = redirect(url_for('index'))
+        response.set_cookie('cart', '', expires=0)
+        return response
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
