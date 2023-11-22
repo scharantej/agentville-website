af062d1cbce19f19314b4f7438587f736fa083a5
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-22 08:47:58 +0530

    Update kidzfeed/main.py

--- kidzfeed/main.py
+++ kidzfeed/main.py
@@ -1,59 +1,37 @@
  
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kids_news.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' % self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/articles')
-def articles():
-    articles = [
-        {
-            'title': 'Article 1',
-            'content': 'This is the content of article 1.'
-        },
-        {
-            'title': 'Article 2',
-            'content': 'This is the content of article 2.'
-        },
-        {
-            'title': 'Article 3',
-            'content': 'This is the content of article 3.'
-        }
-    ]
-    return render_template('articles.html', articles=articles)
+    articles = Article.query.all()
+    return render_template('index.html', articles=articles)
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
-    article = {
-        'title': 'Article 1',
-        'content': 'This is the content of article 1.'
-    }
+    article = Article.query.get(article_id)
     return render_template('article.html', article=article)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == 'secret':
-            return redirect(url_for('index'))
-        else:
-            return redirect(url_for('login'))
-    else:
-        return render_template('login.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Save the user to the database
-        return redirect(url_for('index'))
-    else:
-        return render_template('signup.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
