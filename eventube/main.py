fabd8cf6d8c0b74d96a5e70449b9f32ed5251ded
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-22 09:41:47 +0530

    Update eventube/main.py

--- eventube/main.py
+++ eventube/main.py
@@ -7,42 +7,22 @@ app = Flask(__name__)
 def index():
     articles = [
         {
-            'title': 'New Music from Billie Eilish',
-            'content': 'Billie Eilish has released a new single, "Everything I Wanted." The song is about her relationship with her brother, Finneas O'Connell.',
-            'url': 'https://www.billboard.com/articles/news/9360170/billie-eilish-everything-i-wanted-song-lyrics-meaning'
+            'title': 'New Music Festival Announced for Teenagers',
+            'content': 'A new music festival for teenagers has been announced. The festival will be held in July in Los Angeles, California. The lineup includes some of the biggest names in teen music, including Justin Bieber, Ariana Grande, and Shawn Mendes.',
         },
         {
-            'title': 'Shawn Mendes and Camila Cabello Release New Song',
-            'content': 'Shawn Mendes and Camila Cabello have released a new song, "Señorita." The song is a duet about a summer romance.',
-            'url': 'https://www.rollingstone.com/music/music-news/shawn-mendes-camila-cabello-senorita-song-lyrics-meaning-967473/'
+            'title': 'Teenagers React to New Music Video',
+            'content': 'A new music video from a popular teen artist has been released. The video has been met with mixed reactions from teenagers. Some teenagers love the video, while others think it is too provocative.',
         },
         {
-            'title': 'Taylor Swift Announces New Album',
-            'content': 'Taylor Swift has announced her new album, "Lover." The album is set to be released on August 23rd.',
-            'url': 'https://www.usatoday.com/story/entertainment/music/2019/06/13/taylor-swift-announces-new-album-lover-release-date-tracklist/1124266001/'
-        }
+            'title': 'Teenagers Speak Out About Their Favorite Music',
+            'content': 'A new survey has revealed the favorite music of teenagers. The survey found that pop music is the most popular genre among teenagers, followed by hip-hop and rock music.',
+        },
     ]
     return render_template('index.html', articles=articles)
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
-    articles = [
-        {
-            'title': 'New Music from Billie Eilish',
-            'content': 'Billie Eilish has released a new single, "Everything I Wanted." The song is about her relationship with her brother, Finneas O'Connell.',
-            'url': 'https://www.billboard.com/articles/news/9360170/billie-eilish-everything-i-wanted-song-lyrics-meaning'
-        },
-        {
-            'title': 'Shawn Mendes and Camila Cabello Release New Song',
-            'content': 'Shawn Mendes and Camila Cabello have released a new song, "Señorita." The song is a duet about a summer romance.',
-            'url': 'https://www.rollingstone.com/music/music-news/shawn-mendes-camila-cabello-senorita-song-lyrics-meaning-967473/'
-        },
-        {
-            'title': 'Taylor Swift Announces New Album',
-            'content': 'Taylor Swift has announced her new album, "Lover." The album is set to be released on August 23rd.',
-            'url': 'https://www.usatoday.com/story/entertainment/music/2019/06/13/taylor-swift-announces-new-album-lover-release-date-tracklist/1124266001/'
-        }
-    ]
     article = articles[article_id]
     return render_template('article.html', article=article)
 
@@ -51,4 +31,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
