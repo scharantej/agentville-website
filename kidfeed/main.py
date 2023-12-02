385a98e9c054ce6b313c87b317594921b965d044
scharantej <119601237+scharantej@users.noreply.github.com>
2023-12-02 07:32:01 +0530

    Update kidfeed/main.py

--- kidfeed/main.py
+++ kidfeed/main.py
@@ -1,59 +1,53 @@
  
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsfeed.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' % self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    articles = Article.query.all()
+    return render_template('index.html', articles=articles)
 
-@app.route('/articles')
-def articles():
-    articles = [
-        {
-            'title': 'Article 1',
-            'content': 'This is the content of article 1.'
-        },
-        {
-            'title': 'Article 2',
-            'content': 'This is the content of article 2.'
-        },
-        {
-            'title': 'Article 3',
-            'content': 'This is the content of article 3.'
-        }
-    ]
-    return render_template('articles.html', articles=articles)
-
-@app.route('/article/<int:article_id>')
-def article(article_id):
-    article = {
-        'title': 'Article 1',
-        'content': 'This is the content of article 1.'
-    }
+@app.route('/article/<int:id>')
+def article(id):
+    article = Article.query.get(id)
     return render_template('article.html', article=article)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == 'secret':
-            return redirect(url_for('index'))
-        else:
-            return redirect(url_for('login'))
-    else:
-        return render_template('login.html')
+@app.route('/saved_articles')
+def saved_articles():
+    articles = Article.query.filter_by(saved=True).all()
+    return render_template('saved_articles.html', articles=articles)
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Save the user to the database
-        return redirect(url_for('index'))
-    else:
-        return render_template('signup.html')
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/save_article/<int:id>')
+def save_article(id):
+    article = Article.query.get(id)
+    article.saved = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/unsave_article/<int:id>')
+def unsave_article(id):
+    article = Article.query.get(id)
+    article.saved = False
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
