878b9ecffdd3cf222fc31859c690175878085d4c
scharantej <119601237+scharantej@users.noreply.github.com>
2023-12-02 23:51:56 +0530

    Update frugalizer8723/main.py

--- frugalizer8723/main.py
+++ frugalizer8723/main.py
@@ -8,20 +8,13 @@ db = SQLAlchemy(app)
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    amount = db.Column(db.Float, unique=False, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship('Category', backref=db.backref('expenses', lazy=True))
+    name = db.Column(db.String(80), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
         return '<Expense %r>' % self.name
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Category %r>' % self.name
 
 @app.route('/')
 def index():
@@ -33,67 +26,32 @@ def add_expense():
     if request.method == 'POST':
         name = request.form['name']
         amount = request.form['amount']
-        category_id = request.form['category_id']
-        expense = Expense(name=name, amount=amount, category_id=category_id)
+        category = request.form['category']
+        date = request.form['date']
+        expense = Expense(name=name, amount=amount, category=category, date=date)
         db.session.add(expense)
         db.session.commit()
         return redirect(url_for('index'))
-    else:
-        categories = Category.query.all()
-        return render_template('add_expense.html', categories=categories)
+    return render_template('add_expense.html')
 
-@app.route('/edit_expense/<int:expense_id>', methods=['GET', 'POST'])
-def edit_expense(expense_id):
-    expense = Expense.query.get(expense_id)
+@app.route('/edit_expense/<int:id>', methods=['GET', 'POST'])
+def edit_expense(id):
+    expense = Expense.query.get_or_404(id)
     if request.method == 'POST':
         expense.name = request.form['name']
         expense.amount = request.form['amount']
-        expense.category_id = request.form['category_id']
+        expense.category = request.form['category']
+        expense.date = request.form['date']
         db.session.commit()
         return redirect(url_for('index'))
-    else:
-        categories = Category.query.all()
-        return render_template('edit_expense.html', expense=expense, categories=categories)
+    return render_template('edit_expense.html', expense=expense)
 
-@app.route('/delete_expense/<int:expense_id>')
-def delete_expense(expense_id):
-    expense = Expense.query.get(expense_id)
+@app.route('/delete_expense/<int:id>')
+def delete_expense(id):
+    expense = Expense.query.get_or_404(id)
     db.session.delete(expense)
     db.session.commit()
     return redirect(url_for('index'))
-
-@app.route('/categories')
-def categories():
-    categories = Category.query.all()
-    return render_template('categories.html', categories=categories)
-
-@app.route('/add_category', methods=['GET', 'POST'])
-def add_category():
-    if request.method == 'POST':
-        name = request.form['name']
-        category = Category(name=name)
-        db.session.add(category)
-        db.session.commit()
-        return redirect(url_for('categories'))
-    else:
-        return render_template('add_category.html')
-
-@app.route('/edit_category/<int:category_id>', methods=['GET', 'POST'])
-def edit_category(category_id):
-    category = Category.query.get(category_id)
-    if request.method == 'POST':
-        category.name = request.form['name']
-        db.session.commit()
-        return redirect(url_for('categories'))
-    else:
-        return render_template('edit_category.html', category=category)
-
-@app.route('/delete_category/<int:category_id>')
-def delete_category(category_id):
-    category = Category.query.get(category_id)
-    db.session.delete(category)
-    db.session.commit()
-    return redirect(url_for('categories'))
 
 if __name__ == '__main__':
     db.create_all()
