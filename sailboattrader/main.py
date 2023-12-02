22121159588d87c3bfee4ed3234c71bd557c4911
scharantej <119601237+scharantej@users.noreply.github.com>
2023-12-02 08:32:53 +0530

    Update sailboattrader/main.py

--- sailboattrader/main.py
+++ sailboattrader/main.py
@@ -7,39 +7,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///boats.db'
 db = SQLAlchemy(app)
 
 class Boat(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    contact_info = db.Column(db.String(255), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), unique=True, nullable=False)
+  image_url = db.Column(db.String(255), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  contact_info = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return '<Boat %r>' % self.name
+  def __repr__(self):
+    return '<Boat %r>' % self.name
 
-@app.route('/')
+@app.route("/")
 def index():
-    boats = Boat.query.all()
-    return render_template('index.html', boats=boats)
+  boats = Boat.query.all()
+  return render_template("index.html", boats=boats)
 
-@app.route('/boats')
-def boats():
-    boats = Boat.query.all()
-    return render_template('boats.html', boats=boats)
-
-@app.route('/boats/<int:boat_id>')
+@app.route("/boats/<int:boat_id>")
 def boat_detail(boat_id):
-    boat = Boat.query.get(boat_id)
-    return render_template('boat_detail.html', boat=boat)
+  boat = Boat.query.get(boat_id)
+  return render_template("boat_detail.html", boat=boat)
 
-@app.route('/contact_us', methods=['GET', 'POST'])
+@app.route("/contact_us")
 def contact_us():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        # Send the email
-        return render_template('contact_us.html', message_sent=True)
-    else:
-        return render_template('contact_us.html', message_sent=False)
+  return render_template("contact_us.html")
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+  app.run(debug=True)
