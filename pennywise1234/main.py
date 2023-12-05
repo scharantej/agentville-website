95e4debd91b68ba9e651ed98858246055d111da0
scharantej <119601237+scharantej@users.noreply.github.com>
2023-12-05 22:20:12 +0530

    Update pennywise1234/main.py

--- pennywise1234/main.py
+++ pennywise1234/main.py
@@ -1,31 +1,118 @@
  
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    amount = db.Column(db.Float)
+    category = db.Column(db.String(20))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('expenses', lazy=True))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'], method='sha256')
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Login failed. Check username and/or password')
+    return render_template('login.html')
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
 @app.route('/add_expense', methods=['GET', 'POST'])
+@login_required
 def add_expense():
     if request.method == 'POST':
-        amount = request.form.get('amount')
-        category = request.form.get('category')
-        date = request.form.get('date')
+        title = request.form['title']
+        amount = request.form['amount']
+        category = request.form['category']
+        new_expense = Expense(title=title, amount=amount, category=category, user_id=current_user.id)
+        db.session.add(new_expense)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add_expense.html')
 
-        # Add the expense to the database.
+@app.route('/edit_expense/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_expense(id):
+    expense = Expense.query.get_or_404(id)
+    if request.method == 'POST':
+        expense.title = request.form['title']
+        expense.amount = request.form['amount']
+        expense.category = request.form['category']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit_expense.html', expense=expense)
 
-        return redirect(url_for('view_expenses'))
-    else:
-        return render_template('add_expense.html')
+@app.route('/delete_expense/<int:id>')
+@login_required
+def delete_expense(id):
+    expense = Expense.query.get_or_404(id)
+    db.session.delete(expense)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route('/view_expenses')
+@login_required
 def view_expenses():
-    # Get all expenses from the database.
-
+    expenses = Expense.query.filter_by(user_id=current_user.id).all()
     return render_template('view_expenses.html', expenses=expenses)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
